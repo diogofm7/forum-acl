7f06dedf98036616f36d7554c5d9a23e25b7499b
Diogo Ferreira <diogo.f.m.7@gmail.com>
2020-07-07 14:33:53 -0300

    Aula 01 até Aula 09

---
+++ app/Reply.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Reply extends Model
+{
+    protected $fillable = ['reply'];
+
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+}
